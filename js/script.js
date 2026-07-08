16eac1ba99443082d14eba31b75460766d7f1a86
Wolf Huy <tonewolf41@gmail.com>
2026-07-08 11:14:18 +0700

    Sprint 7: Learn DOM events and conditional logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("Project Upgrade Started");
+let username = "Wolf Huy";
+console.log(username);
+let title = document.querySelector(".title");
+title.textContent = "Project Upgrade v0.2";
+console.log(title);
+let vocabCheckbox = document.querySelector("#vocab");
+vocabCheckbox.addEventListener("change", function() {
+    console.log("Vocabulary checkbox changed!");
+});
+let missionTitle = document.querySelector(".daymiss");
+vocabCheckbox.addEventListener("change", function() {
+    if (vocabCheckbox.checked) {
+        missionTitle.textContent = "Today's Mission: ✅";
+    }
+    else {
+        missionTitle.textContent = "Today's Mission: ";
+    }
+});
