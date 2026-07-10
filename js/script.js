5ff0a82498825d32e5d82f7b29ee9d1d3788da11
Wolf Huy <tonewolf41@gmail.com>
2026-07-10 10:20:01 +0700

    Sprint 9: Save mission progress with localStorage

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
-console.log("Project Upgrade Started");
+
 let username = "Wolf Huy";
-console.log(username);
+
 let title = document.querySelector(".title");
 title.textContent = "Project Upgrade v0.2";
 console.log(title);
-let vocabCheckbox = document.querySelector("#vocab");
-vocabCheckbox.addEventListener("change", function() {
-    console.log("Vocabulary checkbox changed!");
-});
-let gramCheckbox = document.querySelector("#gram");
-let listCheckbox = document.querySelector("#list");
-let wriCheckbox = document.querySelector("#wri");
+
+
 let progress = document.querySelector(".progress");
-console.log(progress.textContent);  
+  
 let checkboxes = document.querySelectorAll("input");
-console.log(checkboxes);
-for (let i = 0; i < 4; i++) {
+
+for (let i = 0; i < checkboxes.length; i++) {
     console.log(checkboxes[i].checked);
 }
 function updateProgress() {
     let completed = 0;
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked) {
         completed++;
     }
 }
 progress.textContent = `Progress ${completed}/${checkboxes.length}`;
-}
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < checkboxes.length; i++) {
+    localStorage.setItem(
+        checkboxes[i].id, 
+        checkboxes[i].checked);
+}}
+for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener("change", updateProgress);
 }
+for (let i = 0; i < checkboxes.length; i++) {
+    if (localStorage.getItem(checkboxes[i].id) === "true") {
+        checkboxes[i].checked = true;
+    }
+}
+updateProgress();
--- js/useless.js
+++ js/useless.js
@@ -6,3 +6,40 @@ vocabCheckbox.addEventListener("change", function() {
     else {
         missionTitle.textContent = "Today's Mission: ";
     }
+});
+
+function updateProgress() {
+    let completed = 0;
+for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+        completed++;
+    }
+}
+progress.textContent = `Progress ${completed}/${checkboxes.length}`;
+for (let i = 0; i < checkboxes.length; i++) {
+    localStorage.setItem(checkboxes[i].id, checkboxes[i].checked);
+}}
+for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener("change", updateProgress);
+}
+for (let i = 0; i < checkboxes.length; i++) {
+    if (localStorage.getItem(checkboxes[i].id) === "true") {
+        checkboxes[i].checked = true;
+    }
+}
+
+
+
+function updateProgress() {
+    let completed = 0;
+for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+        completed++;
+    }
+    localStorage.setItem(
+        checkboxes[i].id, 
+        checkboxes[i].checked
+    );
+progress.textContent = `Progress ${completed}/${checkboxes.length}`;
+}}
+updateProgress();
